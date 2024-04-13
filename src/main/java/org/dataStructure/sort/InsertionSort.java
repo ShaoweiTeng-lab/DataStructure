5548b2a2fd128ca54700f1f8d6d26c093d595985
鄧紹偉 <sdonline13@gmail.com>
2024-04-13 10:58:17 +0800

    add 遞迴

--- src/main/java/org/dataStructure/sort/InsertionSort.java
+++ src/main/java/org/dataStructure/sort/InsertionSort.java
@@ -7,14 +7,14 @@ package org.dataStructure.sort;
 public class InsertionSort {
     public static void main(String[] args) {
         int [] array = new int[]{10 ,7,11,23,1,0};
-        for(int i = 0 ; i< array.length-1 ; i++){
-            for(int j = i - 1 ; j >= 0 ; j--){
-                if(array[j+1] > array[j]){
-                    int temp = array[j];
-                    array[j] = array[ j +1 ];
-                    array[ j +1 ] = temp;
+        for(int i_start = 0 ; i_start< array.length-1 ; i_start++){
+            for(int j_run = i_start - 1 ; j_run >= 0 ; j_run--){
+                if(array[j_run+1] > array[j_run]){
+                    int temp = array[j_run];
+                    array[j_run] = array[ j_run +1 ];
+                    array[ j_run +1 ] = temp;
                 }
-                else  // 當發現左邊比較大時 中止內部回權
+                else  // 當發現左邊比較大時 中止內部回圈
                     break;
             }
         }
